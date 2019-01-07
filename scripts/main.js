5ad6bead80a1c6ed9497b47c8e2e730221a6e136
illges <brown1ei.eb@gmail.com>
2019-01-07 14:51:55 -0500

    Add Win Conditions

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,55 @@
 const gameBoard = (() => {
-	const add = (e) => {
-		console.log(e.target.textContent)
+	let tiles = [];
+	const add = (tile, index) => {
+		tiles[index] = tile;
+		console.log(tiles);
 	};
-	return { add };
 
+	const checkTiles = () => tiles;
+
+	const initialize = () => tiles = [];
+
+	return { add, checkTiles, initialize };
 })();
 
 const game = (() => {
-	let currentPlayer = "X";
+	let currentPlayer = 'X',
+		gameOver = false,
+		highLight;
+
+	let winningConditions = [
+		[0,1,2], [0,3,6], [0,4,8],
+		[1,4,7], [2,4,6], [3,4,5],
+		[2,5,8], [6,7,8]
+	]
 
 	const changePlayer = () => currentPlayer = currentPlayer === "X" ? "O" : "X";
 
 	const move = () => {
 		Array.from(document.querySelectorAll('.tile')).forEach(i => { 
 			i.addEventListener('click', (e) => {
-				if (!i.textContent) {
+				if (!i.textContent && !gameOver) {
 					i.textContent = currentPlayer
+					gameBoard.add(currentPlayer, i.dataset.tile);
+					checkGameOver()
 					changePlayer()
-					gameBoard.add(e)
 				}
 			})
 		})
 	};
 
-	const reset = () => document.querySelector('.reset').addEventListener('click', () => {
-		Array.from(document.querySelectorAll('.tile')).forEach(i => {
-			i.textContent = ''
+	const checkGameOver = () => {
+		let tiles = gameBoard.checkTiles();
+		winningConditions.some(i => {
+			if(tiles[i[0]] === tiles[i[1]] && tiles[i[1]] === tiles[i[2]] && tiles[i[1]]) {
+				gameOver = true;
+				console.log('GAME OVER NOOB!')
+			}
 		})
-	})
-	return { move }
-})();
+	}
 
-const player = (name, team) => {
-	return { name, team }
-}
+	return { move, gameOver, currentPlayer }
+})();
 
 const displayController = (() => {
 
@@ -54,7 +70,16 @@ const displayController = (() => {
 		restartButton = document.createElement('button');
 		restartButton.setAttribute('id', 'restart');
 		restartButton.textContent = 'Restart';
-		restartButton.addEventListener('click', (e) => console.log(e))
+		restartButton.addEventListener('click', (e) => {
+			Array.from(document.querySelectorAll('.tile')).forEach(i => {
+				i.textContent = ''
+			});
+			game.gameOver = false;
+			game.currentPlayer = 'X';
+			gameBoard.initialize();
+			console.log(e.target);
+			game.move();
+		});
 
 		game.appendChild(board);
 		game.appendChild(restartButton);
